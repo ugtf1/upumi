76715e8a338225ce96f05546f37af959ea423080
Chuks Emmanuel Okolo <164390660+Chukseo@users.noreply.github.com>
2026-08-31 15:44:08 +0100

    Set admin member table pagination to 20 items per page

--- web/src/addons/Member.tsx
+++ web/src/addons/Member.tsx
@@ -190,7 +190,7 @@ export default function MemberPage() {
   const menuRef = useRef<HTMLDivElement | null>(null);
 
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10;
+  const itemsPerPage = 20;
   const [isMemberFilterModalOpen, setIsMemberFilterModalOpen] = useState(false);
 
   useEffect(() => {
